b606a9191b7c3f1696b78cb5e39941aebc4dbd32
viztor <viztor@outlook.com>
2014-06-02 00:35:20 +0800

    Enhanced Playlist
    Enhanced Playlist

--- js/playlist.js
+++ js/playlist.js
@@ -1,2 +1,17 @@
-var playlist = 
-['14年PETS-2模拟考场（13年9月PETS2真题）', '14年PETS-2模拟考场（试题28）', '14年PETS-2模拟考场（独白二）', '14年PETS-2模拟考场（14年3月PETS2真题）','14年PETS-2模拟考场（试题29）']
+var playlistPrefix = '14年PETS-2模拟考场（';
+var playlistEndfix = '）';
+var playlistPre = ["试题", "长对话", "短对话", "独白", "语音点拨", "13年9月PETS2真题", "14年3月PETS2真题"];
+var playlistPreCount = [38, 9, 9, 3, 1, 1, 1];
+var ChineseNumber = ['一', '二', '三', '四', '五', '六', '七', '八', '九'];
+var playlist = [];
+var i, j, listCountNum = '', playlistItem = '';
+for (i = 0; i < playlistPre.length; i++) {
+    for (j = 0; j < playlistPreCount[i]; j++) {
+    listCountNum = playlistPreCount[i] < 10 ? ChineseNumber[j] : j+1;
+    listCountNum = playlistPreCount[i] === 1 ? '' : listCountNum;
+    playlistItem = playlistPrefix + playlistPre[i] + listCountNum + playlistEndfix;
+    playlist.push(playlistItem);
+    console.log(playlistItem);
+    }
+}
+console.log(playlist);
